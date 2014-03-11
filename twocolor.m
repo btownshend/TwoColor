6806cf69d5808e7e6c9ee608a277b15d14710a69
Brent Townshend <bst@tc.com>
2014-03-11 16:42:09 -0700

    Several reorgs

--- twocolor.m
+++ twocolor.m
@@ -27,65 +27,74 @@ function f=twocolor(fnames,gates,varargin)
                   'ratiorange',[1e-2,20],...
                   'normalizeratios',false,...
                   'maxevents',1000000,...
-                  'desc',{{}});
+                  'desc',{{}},...
+                  'plotdensity',true,...
+                  'plotratios',true,...
+                  'title','');
   args=processargs(defaults,varargin);
 
-  fprintf('Loading data from %d files\n', length(fnames));
+  if isstruct(fnames)
+    fprintf('Using already loaded data\n');
+    f=fnames;
+    fnames={f.fname};
+  else
+    fprintf('Loading data from %d files\n', length(fnames));
 
-  % Load all data into f and construct gates
-  f=struct([]);
-  
-  
-  for i=1:length(fnames)
-    fprintf('Loading %s...',fnames{i});
-    [unscdata,f(i).hdr,f(i).data]=fca_readfcs(fnames{i});
-    fprintf('read %d events\n', size(f(i).data,1));
-    if size(f(i).data,1)>args.maxevents
-      fprintf('Only keeping %d/%d events\n', args.maxevents,size(f(i).data,1));
-      f(i).data=f(i).data(1:args.maxevents,:);
-    end
-    f(i).fname=fnames{i};
-    if ~isempty(args.desc)
-      f(i).hdr.cells=args.desc{i};
-    elseif isempty(f(i).hdr.cells)
-      f(i).hdr.cells=f(i).fname;
-    end
-    % Channel mapping for devices used
-    fsca=findchannel(f(i).hdr.par,'FSC-A',{'*FSC'},1);
-    ssca=findchannel(f(i).hdr.par,'SSC-A',{'*SSC'},1);
-    sscw=findchannel(f(i).hdr.par,'SSC-W',{},0);
-    ssch=findchannel(f(i).hdr.par,'SSC-H',{},0);
-    fsch=findchannel(f(i).hdr.par,'FSC-H',{},0);
-    gfp=findchannel(f(i).hdr.par,'GFP',{'GFP-A','B1-A','FITC-A','525/50 [488]'},1);
-    cherry=findchannel(f(i).hdr.par,'mCherry-A',{'Y2-A','610/20 [561]'},1);
-    dapi=findchannel(f(i).hdr.par,'DAPI-A',{'V1-A','460/50 [405]'},0);  % Optional 
-    triggerPW=findchannel(f(i).hdr.par,'Trigger Pulse Width',{},0);
-    % Copy channels to explicit fields in f(i)
-    channels={'fsca','fsch','ssca','sscw','ssch','gfp','cherry','dapi','triggerPW'};
-    for j=1:length(channels)
-      channel=channels{j};
-      if ~isempty(eval(channel))
-        f(i).(channel)=f(i).data(:,eval(channel));
-      end
-    end
-    if ~isfield(f(i),'dapi') || isempty(f(i).dapi)
-      % Kludge - fake DAPI as 300.0 so gates work
-      f(i).dapi=300*ones(size(f(i).fsca));
-    end
+    % Load all data into f and construct gates
+    f=struct([]);
     
-    f(i).ratio=f(i).gfp./f(i).cherry;
+    
+    for i=1:length(fnames)
+      fprintf('Loading %s...',fnames{i});
+      [unscdata,f(i).hdr,f(i).data]=fca_readfcs(fnames{i});
+      fprintf('read %d events\n', size(f(i).data,1));
+      if size(f(i).data,1)>args.maxevents
+        fprintf('Only keeping %d/%d events\n', args.maxevents,size(f(i).data,1));
+        f(i).data=f(i).data(1:args.maxevents,:);
+      end
+      f(i).fname=fnames{i};
+      if ~isempty(args.desc)
+        f(i).hdr.cells=args.desc{i};
+      elseif isempty(f(i).hdr.cells)
+        f(i).hdr.cells=f(i).fname;
+      end
+      % Channel mapping for devices used
+      fsca=findchannel(f(i).hdr.par,'FSC-A',{'*FSC'},1);
+      ssca=findchannel(f(i).hdr.par,'SSC-A',{'*SSC'},1);
+      sscw=findchannel(f(i).hdr.par,'SSC-W',{},0);
+      ssch=findchannel(f(i).hdr.par,'SSC-H',{},0);
+      fsch=findchannel(f(i).hdr.par,'FSC-H',{},0);
+      gfp=findchannel(f(i).hdr.par,'GFP',{'GFP-A','B1-A','FITC-A','525/50 [488]'},1);
+      cherry=findchannel(f(i).hdr.par,'mCherry-A',{'Y2-A','610/20 [561]'},1);
+      dapi=findchannel(f(i).hdr.par,'DAPI-A',{'V1-A','460/50 [405]'},0);  % Optional 
+      triggerPW=findchannel(f(i).hdr.par,'Trigger Pulse Width',{},0);
+      % Copy channels to explicit fields in f(i)
+      channels={'fsca','fsch','ssca','sscw','ssch','gfp','cherry','dapi','triggerPW'};
+      for j=1:length(channels)
+        channel=channels{j};
+        if ~isempty(eval(channel))
+          f(i).(channel)=f(i).data(:,eval(channel));
+        end
+      end
+      if ~isfield(f(i),'dapi') || isempty(f(i).dapi)
+        % Kludge - fake DAPI as 300.0 so gates work
+        f(i).dapi=300*ones(size(f(i).fsca));
+      end
+      
+      f(i).ratio=f(i).gfp./f(i).cherry;
 
 
-    % Form gates
-    f(i).P=gates.applyall(f(i));
-    gates.printstats(f(i));
+      % Form gates
+      f(i).P=gates.applyall(f(i));
+      gates.printstats(f(i));
+    end
   end
-
+  
   usegatenum=gates.lookup(args.usegate);
   fprintf('Using gate %s (%d)\n', args.usegate,usegatenum);
   
   % Print data summary
-  fprintf(' i           Condition                       Count      GFP   resid  cherry   ratio   resid       m       b   resid GFP(600)\n');
+  fprintf(' i           Condition                       Count      GFP   resid  cherry   ratio  stdev   resid       m       b   resid GFP(600)\n');
   for i=1:length(f)
     hdr=f(i).hdr;
     P=f(i).P;
@@ -94,9 +103,10 @@ function f=twocolor(fnames,gates,varargin)
     m=pf(1);b=pf(2);
     mgfp=median(f(i).gfp(P(usegatenum,:)));
     mcherry=median(f(i).cherry(P(usegatenum,:)));
-    ratio=mgfp./mcherry;
-    rresid=f(i).gfp(P(usegatenum,:))-ratio*f(i).cherry(P(usegatenum,:));
-    fprintf('%2d %40s %6d  %7.1f %7.1f %7.1f %7.3f %7.1f %7.3f %7.1f %7.1f %7.1f\n',i,hdr.cells,sum(P(usegatenum,:)),mgfp,std(f(i).gfp(P(usegatenum,:))),mcherry,ratio,std(rresid),m,b,std(presid),m*600+b);
+    ratio=f(i).gfp(P(usegatenum,:))./f(i).cherry(P(usegatenum,:));
+    stdev=std(log(ratio((ratio/median(ratio))>0.25&(ratio/median(ratio))<4)));
+    rresid=f(i).gfp(P(usegatenum,:))-median(ratio)*f(i).cherry(P(usegatenum,:));
+    fprintf('%2d %40s %6d  %7.1f %7.1f %7.1f %7.3f %7.3f %7.1f %7.3f %7.1f %7.1f %7.1f\n',i,hdr.cells,sum(P(usegatenum,:)),mgfp,std(f(i).gfp(P(usegatenum,:))),mcherry,median(ratio),exp(stdev),std(rresid),m,b,std(presid),m*600+b);
   end
 
   if 0
@@ -112,117 +122,131 @@ function f=twocolor(fnames,gates,varargin)
     title(sprintf('DAPI Live/Dead Theshold (%.0f)',args.dapithresh));
   end
 
-  % Plot all the density of GFP vs. mCherry
-  setfig([f(1).fname,' GFPvsCherry']); clf;
-  lastcells='';
+  if args.plotdensity
+    % Plot all the density of GFP vs. mCherry
+    setfig([f(1).fname,' GFPvsCherry']); clf;
+    lastcells='';
 
-  slow=1e10; shigh=1;
-  glow=1e10; ghigh=1;
-  for i=1:length(f)
-    sel=f(i).P(usegatenum,:);
-    sel=f(i).cherry>0 & f(i).gfp>0;
-    sch=sort(f(i).cherry(sel));slow=min(slow,10^floor(log10(sch(round(length(sch)*.02)))));shigh=max(shigh,10^ceil(log10(sch(round(length(sch)*.98)))));
-    sgfp=sort(f(i).gfp(sel));glow=min(glow,10^floor(log10(sgfp(round(length(sgfp)*.02)))));ghigh=max(ghigh,10^ceil(log10(sgfp(round(length(sgfp)*.98)))));
+    slow=1e10; shigh=1;
+    glow=1e10; ghigh=1;
+    for i=1:length(f)
+      sel=f(i).P(usegatenum,:);
+      sel=f(i).cherry>0 & f(i).gfp>0;
+      sch=sort(f(i).cherry(sel));slow=min(slow,10^floor(log10(sch(round(length(sch)*.02)))));shigh=max(shigh,10^ceil(log10(sch(round(length(sch)*.98)))));
+      sgfp=sort(f(i).gfp(sel));glow=min(glow,10^floor(log10(sgfp(round(length(sgfp)*.02)))));ghigh=max(ghigh,10^ceil(log10(sgfp(round(length(sgfp)*.98)))));
+    end
+    
+    for i=1:length(f)
+      sel=f(i).P(usegatenum,:);
+      %    sel=f(i).cherry>0 & f(i).gfp>0;
+      gfpval=exp(mean(log(f(i).gfp(sel))));
+
+      val=sum(f(i).gfp(sel))/sum(f(i).cherry(sel));
+      subplot(ceil(length(f)/2),1+(length(f)>1),i);
+      densplot(f(i).cherry(sel),f(i).gfp(sel),[],[slow shigh glow ghigh],1);
+      xlabel('mCherry');
+      ylabel('GFP');
+      info=sprintf('%16s G=%4.0f G/C=%4.3g ',f(i).hdr.cells,gfpval,val);
+      title(info,'Interpreter','none');
+      disp([fnames{i},': ',info])
+      lastcells=f(i).hdr.cells;
+    end
+    suptitle(strrep(args.title,'_','-'));
   end
-  
-  for i=1:length(f)
-    sel=f(i).P(usegatenum,:);
-    sel=f(i).cherry>0 & f(i).gfp>0;
-    gfpval=exp(mean(log(f(i).gfp(sel))));
 
-    val=sum(f(i).gfp(sel))/sum(f(i).cherry(sel));
-    subplot(ceil(length(f)/2),1+(length(f)>1),i);
-    densplot(f(i).cherry(sel),f(i).gfp(sel),[],[slow shigh glow ghigh],1);
-    xlabel('mCherry');
-    ylabel('GFP');
-    info=sprintf('%16s G=%4.0f G/C=%4.3g ',f(i).hdr.cells,gfpval,val);
-    title(info,'Interpreter','none');
-    disp([fnames{i},': ',info])
-    lastcells=f(i).hdr.cells;
-  end
+  if args.plotratios
+    % Plot the GFP/mCherry ratios
+    legh=[];
+    legv={};
+    ti='';
 
+    setfig([f(1).fname,' Bin edges']);clf;
 
-  % Plot the GFP/mCherry ratios
-  legh=[];
-  legv={};
-  ti='';
-
-  setfig([f(1).fname,' Bin edges']);clf;
-
-  edges=[];
-  if ~isempty(args.bins)
-    edges=args.bins;
-  elseif ~isempty(args.maingates)
-    for i=1:length(args.maingates)
-      edges(i*2-1)=args.maingates{i}(1);
-      edges(i*2)=args.maingates{i}(2);
+    edges=[];
+    if ~isempty(args.bins)
+      edges=args.bins;
+    elseif ~isempty(args.maingates)
+      for i=1:length(args.maingates)
+        edges(i*2-1)=args.maingates{i}(1);
+        edges(i*2)=args.maingates{i}(2);
+      end
+    elseif args.sortbins>0 && sum(f(i).P(usegatenum,:))>100
+      sortratio=sort(f(i).ratio(f(i).P(usegatenum,:)));
+      midratio=exp(mean(log(sortratio(round([.05,.95]*length(sortratio))))));
+      fprintf('Mid ratio = %g\n', midratio);
+      if isempty(args.firstbin)
+        low=sortratio(sortratio<midratio);
+        args.firstbin=low(round(0.5*length(low)));
+        fprintf('First bin at %g\n',args.firstbin);
+      end
+      if isempty(args.lastbin)
+        high=sortratio(sortratio>midratio);
+        args.lastbin=high(round(0.90*length(high)));
+        fprintf('Last bin at %g\n',args.lastbin);
+      end
+      edges=args.firstbin*(args.lastbin/args.firstbin).^((0:(args.sortbins-2))/(args.sortbins-2));
     end
-  elseif args.sortbins>0
-    sortratio=sort(f(i).ratio(f(i).P(usegatenum,:)));
-    midratio=exp(mean(log(sortratio(round([.05,.95]*length(sortratio))))));
-    fprintf('Mid ratio = %g\n', midratio);
-    if isempty(args.firstbin)
-      low=sortratio(sortratio<midratio);
-      args.firstbin=low(round(0.5*length(low)));
-      fprintf('First bin at %g\n',args.firstbin);
+    
+    
+    cols='bcmyrg';
+    for i=1:length(f)
+      x=f(i).data;
+      hdr=f(i).hdr;
+      P=f(i).P;
+      ratio=f(i).ratio(P(usegatenum,:));
+      f(i).N=length(ratio);
+      if length(ratio)<100
+        f(i).mu=nan;
+        f(i).sigma=nan;
+        continue;
+      end
+      second=0;
+      if ~isempty([strfind(hdr.cells,'+theo'),strfind(hdr.cells,'+ theo'),strfind(f(i).fname,'+theo')])
+        col='g';
+      elseif ~isempty([strfind(hdr.cells,'-theo'),strfind(hdr.cells,'- theo'),strfind(f(i).fname,'-theo')])
+        col='r';
+      else
+        col=cols(mod(i-1,length(cols))+1);
+      end
+      if args.normalizeratios
+        legh(end+1)=pdfplot(ratio/median(ratio),col,1,round(length(ratio)/200));
+      else
+        legh(end+1)=pdfplot(ratio,col,1,round(length(ratio)/200));
+      end
+      hold on;
+      % Calculate std of log(normal) distribution with extreme points removed
+      stdev=std(log(ratio((ratio/median(ratio))>0.25&(ratio/median(ratio))<4)));
+      legv{end+1}=sprintf('%s (mu=%.3f,sigma=%.2f,N=%d)',hdr.cells,median(ratio),exp(stdev),length(ratio));
+      f(i).mu=median(ratio);
+      f(i).sigma=exp(stdev);
+      ti=[ti,', ',hdr.cells];
     end
-    if isempty(args.lastbin)
-      high=sortratio(sortratio>midratio);
-      args.lastbin=high(round(0.90*length(high)));
-      fprintf('Last bin at %g\n',args.lastbin);
-    end
-    edges=args.firstbin*(args.lastbin/args.firstbin).^((0:(args.sortbins-2))/(args.sortbins-2));
-  end
-  
-  
-  cols='bcmyrg';
-  for i=1:length(f)
-    x=f(i).data;
-    hdr=f(i).hdr;
-    P=f(i).P;
-    ratio=f(i).ratio(P(usegatenum,:));
-    if length(ratio)<100
-      continue;
-    end
-    second=0;
-    if ~isempty([strfind(hdr.cells,'+theo'),strfind(hdr.cells,'+ theo'),strfind(f(i).fname,'+theo')])
-      col='g';
-    elseif ~isempty([strfind(hdr.cells,'-theo'),strfind(hdr.cells,'- theo'),strfind(f(i).fname,'-theo')])
-      col='r';
+    ti=ti(3:end);  % Remove leading ', '
+    if ~isempty(edges)
+      f(i).edges=edges;
+      fprintf('Edges for %s are [%s]\nPcts: ',hdr.cells,sprintf('%.2g ',edges));
+      cum=0;
+      c=axis;
+      for k=1:length(edges)
+        plot([edges(k),edges(k)],c(3:4),':');
+        fprintf('%.1f%% ',(sum(ratio<edges(k))-cum)/length(ratio)*100);
+        cum=sum(ratio<edges(k));
+      end
+      fprintf('%.1f%%\n',(length(ratio)-cum)/length(ratio)*100);
     else
-      col=cols(mod(i-1,length(cols))+1);
+      f(i).edges=nan;
     end
-    if args.normalizeratios
-      legh(end+1)=pdfplot(ratio/median(ratio),col,1,round(length(ratio)/200));
-    else
-      legh(end+1)=pdfplot(ratio,col,1,round(length(ratio)/200));
-    end
-    hold on;
-    % Calculate std of log(normal) distribution with extreme points removed
-    stdev=std(log(ratio((ratio/median(ratio))>0.25&(ratio/median(ratio))<4)));
-    legv{end+1}=sprintf('%s (mu=%.3f,sigma=%.2f,N=%d)',hdr.cells,median(ratio),exp(stdev),length(ratio));
-    ti=[ti,', ',hdr.cells];
-  end
-  ti=ti(3:end);  % Remove leading ', '
-  if ~isempty(edges)
-    f(i).edges=edges;
-    fprintf('Edges for %s are [%s]\nPcts: ',hdr.cells,sprintf('%.2g ',edges));
-    cum=0;
+
     c=axis;
-    for k=1:length(edges)
-      plot([edges(k),edges(k)],c(3:4),':');
-      fprintf('%.1f%% ',(sum(ratio<edges(k))-cum)/length(ratio)*100);
-      cum=sum(ratio<edges(k));
+    c(1:2)=args.ratiorange;
+    axis(c);
+    xlabel('GFP/mCherry');
+    if ~isempty(args.title)
+      ti=args.title;
     end
-    fprintf('%.1f%%\n',(length(ratio)-cum)/length(ratio)*100);
+    title(ti,'Interpreter','none');
+    legend(legh,legv,'Interpreter','none','Location','NorthWest');
   end
-
-  c=axis;
-  c(1:2)=args.ratiorange;
-  axis(c);
-  xlabel('GFP/mCherry');
-  title(ti,'Interpreter','none');
-  legend(legh,legv,'Interpreter','none');
 end
 
 % Locate which channel number corresponds to a particular field
