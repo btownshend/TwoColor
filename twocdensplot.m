34d1604f03285adf7660fdcd7d76b9cdee843029
Brent Townshend <bst@tc.com>
2014-09-10 00:46:59 -0700

    Minor mods to twocdensplot.m

--- twocdensplot.m
+++ twocdensplot.m
@@ -1,6 +1,6 @@
 function twocdensplot(f,ti)
 % Plot all the density of GFP vs. mCherry
-setfig([f(1).fname,' GFPvsCherry']); clf;
+setfig([f(1).fname,' ',ti,' GFPvsCherry']); clf;
 lastcells='';
 
 slow=1e10; shigh=1;
@@ -17,12 +17,14 @@ for i=1:length(f)
   %    sel=f(i).cherry>0 & f(i).gfp>0;
   gfpval=exp(mean(log(f(i).gfp(sel))));
   chval=exp(mean(log(f(i).cherry(sel))));
-
+  ratio=f(i).ratio(f(i).P(f(i).usegatenum,:));
+  mu=median(ratio);
+  
   subplot(ceil(length(f)/2),1+(length(f)>1),i);
   densplot(f(i).cherry(sel),f(i).gfp(sel),[],[slow shigh glow ghigh],1);
   xlabel('mCherry');
   ylabel('GFP');
-  info=sprintf('%16s N=%d G=%4.0f R=%.0f mu=%4.3g ',f(i).hdr.cells,sum(sel),gfpval,chval,f(i).mu);
+  info=sprintf('%16s N=%d G=%4.0f R=%.0f mu=%4.3g ',f(i).hdr.cells,sum(sel),gfpval,chval,mu);
   c=axis;
   r=[max(min(f(i).cherry(sel)),min(f(i).gfp(sel))/f(i).mu),min(max(f(i).cherry(sel)),max(f(i).gfp(sel))/f(i).mu)];
   hold on;
