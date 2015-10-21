890faf394eb79fe3d93ae64d7bc3e42b7ca2d2b7
Brent Townshend <bst@tc.com>
2015-10-21 16:03:54 -0700

    Made into a function with optional args

--- bdfacs_setratios.m
+++ bdfacs_setratios.m
@@ -1,39 +1,41 @@
-in=fopen('12way-macros.xml','r');
-out=fopen('12way-out.xml','w');
-minx=30;
-maxx=262144;
-miny=minx;
-maxy=maxx;
-%elow=10^(3.82172-5.35478);   % Bin 1-2 edge
-%ehigh=10^(5.47689-4.9286);
-elow=10^(3.88548-5.41854);   % Bin 1-2 edge
-ehigh=10^(5.418540-4.87025);
-elow=2;
-ehigh=30;
-ratios=logspace(log10(elow),log10(ehigh),11);
+% Create a BD FACS settings file with a set of ratio gates setup
+% This will create the given number of bins with the lowest and highest bins as
+%  triangular regions enclosing everything below or above the given ratio bounds
+% The remaining pins are strips (actually 4-side polygons) that are equally space% in terms of log(ratio)
+function bdfacs_setratios(nbins,ratiolow,ratiohigh,outfile,varargin)
+defaults=struct('minx',30,'miny',30,'maxx',262144,'maxy',262144);
+args=processargs(defaults,varargin);
+
+macrofilename=sprintf('%dway-macros.xml',nbins);
+in=fopen(macrofilename,'r');
+if in<0
+  error('Unable to open macro file: %s\n', macrofilename);
+end
+out=fopen(outfile,'w');
+ratios=logspace(log10(ratiolow),log10(ratiohigh),11);
 fprintf('Ratios: %s\n', sprintf('%.2f ',ratios));
 while true
   line=fgetl(in);
   if line==-1
     break;
   end
-  line=strrep(line,'##Max.X##',sprintf('%f',log10(maxx)));
-  line=strrep(line,'##Min.X##',sprintf('%f',log10(minx)));
-  line=strrep(line,'##Max.Y##',sprintf('%f',log10(maxy)));
-  line=strrep(line,'##Min.Y##',sprintf('%f',log10(miny)));
+  line=strrep(line,'##Max.X##',sprintf('%f',log10(args.maxx)));
+  line=strrep(line,'##Min.X##',sprintf('%f',log10(args.minx)));
+  line=strrep(line,'##Max.Y##',sprintf('%f',log10(args.maxy)));
+  line=strrep(line,'##Min.Y##',sprintf('%f',log10(args.miny)));
   for i=1:length(ratios)
     r=ratios(i);
     prefix=sprintf('##E%d.%d',i,i+1);
     if r<1
-      line=strrep(line,[prefix,'.low.X##'],sprintf('%f',log10(miny/r)));
-      line=strrep(line,[prefix,'.low.Y##'],sprintf('%f',log10(miny)));
-      line=strrep(line,[prefix,'.high.X##'],sprintf('%f',log10(maxx)));
-      line=strrep(line,[prefix,'.high.Y##'],sprintf('%f',log10(maxx*r)));
+      line=strrep(line,[prefix,'.low.X##'],sprintf('%f',log10(args.miny/r)));
+      line=strrep(line,[prefix,'.low.Y##'],sprintf('%f',log10(args.miny)));
+      line=strrep(line,[prefix,'.high.X##'],sprintf('%f',log10(args.maxx)));
+      line=strrep(line,[prefix,'.high.Y##'],sprintf('%f',log10(args.maxx*r)));
     else
-      line=strrep(line,[prefix,'.low.X##'],sprintf('%f',log10(minx)));
-      line=strrep(line,[prefix,'.low.Y##'],sprintf('%f',log10(minx*r)));
-      line=strrep(line,[prefix,'.high.X##'],sprintf('%f',log10(maxy/r)));
-      line=strrep(line,[prefix,'.high.Y##'],sprintf('%f',log10(maxy)));
+      line=strrep(line,[prefix,'.low.X##'],sprintf('%f',log10(args.minx)));
+      line=strrep(line,[prefix,'.low.Y##'],sprintf('%f',log10(args.minx*r)));
+      line=strrep(line,[prefix,'.high.X##'],sprintf('%f',log10(args.maxy/r)));
+      line=strrep(line,[prefix,'.high.Y##'],sprintf('%f',log10(args.maxy)));
     end
   end
   fprintf(out,'%s\n',line);
